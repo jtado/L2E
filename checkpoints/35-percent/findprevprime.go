4b81d20d996597aab32ea26ed133523cfe8320fe
Israel Olawuyi <Jtado732@gmail.com>
2026-06-03 14:43:28 +0000

    fixed find prev prime

--- checkpoints/35-percent/findprevprime.go
+++ checkpoints/35-percent/findprevprime.go
@@ -1,31 +1,22 @@
 package student
 
-func IsPrime(n int) bool {
-	if n <= 1 {
-		return false
-	}
-	if n <= 3 {
-		return true
-	}
-	if n%2 == 0 || n%3 == 0 {
-		return false
-	}
-	for i := 5; i*i <= n; i += 6 {
-		if n%i == 0 || n%(i+2) == 0 {
-			return false
-		}
-	}
-	return true
-}
-
 func FindPrevPrime(nb int) int {
-	if nb < 2 {
-		return 0
-	}
 	for i := nb; i >= 2; i-- {
-		if IsPrime(i) {
+		if isPrime(i) {
 			return i
 		}
 	}
 	return 0
+}
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
